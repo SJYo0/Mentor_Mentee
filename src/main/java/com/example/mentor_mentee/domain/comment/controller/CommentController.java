44d4bd81edabe34dea4b728f6a2a76fd0f43346c
SJYo0 <wnsdyd9109@gmail.com>
2025-10-11 18:52:30 +0900

    CommentController 임시작성 완료

---
+++ src/main/java/com/example/mentor_mentee/domain/comment/controller/CommentController.java
@@ -0,0 +1,33 @@
+package com.example.mentor_mentee.domain.comment.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    @PostMapping("/")
+    public String CreateComment(String content){
+        return content + "댓글 작성 완료";
+    }
+
+    @GetMapping
+    public String getAllComments(){
+        return "댓글 목록 조회 완료";
+    }
+
+    @GetMapping("/{comment-id}")
+    public String getCommentById(@PathVariable("comment-id") Long id){
+        return id +"번 댓글 조회 완료";
+    }
+
+    @PutMapping("/{comment-id}")
+    public String updateComment(@PathVariable("comment-id") Long id){
+        return id +"번 댓글 수정 완료";
+    }
+
+    @DeleteMapping("/{comment-id}")
+    public String deleteComment(@PathVariable("comment-id") Long id){
+        return id +"번 댓글 삭제 완료";
+    }
+}
