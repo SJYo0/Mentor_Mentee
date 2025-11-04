ad789b731f94156e000644a977ab9c2b6903c1de
SJYo0 <wnsdyd9109@gmail.com>
2025-11-04 19:39:47 +0900

    1104 내용

--- src/main/java/com/example/mentor_mentee/domain/post/entity/Post.java
+++ src/main/java/com/example/mentor_mentee/domain/post/entity/Post.java
@@ -1,10 +1,14 @@
 package com.example.mentor_mentee.domain.post.entity;
 
+import com.example.mentor_mentee.domain.comment.entity.Comment;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,6 +32,10 @@ public class Post {
     @Column(nullable = false)
     @Builder.Default // builder에서 초기값을 지정해줄 수 있게해주는 어노테이션
     private long views = 0L; // 초기값 설정해주기
+
+    @OneToMany(mappedBy = "post") // OneToMany는 지연로딩이 디폴트, cascade 설정도 해줘야됨. orphanRemoval 도 해야됨
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
 
     public void update(String title, String content) {
         this.title = title;
