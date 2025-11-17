f9fb855c84a22ced17f6be1e47ab0ce07a54d293
SJYo0 <wnsdyd9109@gmail.com>
2025-11-17 22:24:12 +0900

    1111 7주차 완료

--- src/main/java/com/example/mentor_mentee/domain/post/entity/Post.java
+++ src/main/java/com/example/mentor_mentee/domain/post/entity/Post.java
@@ -29,10 +29,6 @@ public class Post {
     @Column(nullable = false)
     private String content;
 
-    @Column(nullable = false)
-    @Builder.Default // builder에서 초기값을 지정해줄 수 있게해주는 어노테이션
-    private long views = 0L; // 초기값 설정해주기
-
     @OneToMany(mappedBy = "post") // OneToMany는 지연로딩이 디폴트, cascade 설정도 해줘야됨. orphanRemoval 도 해야됨
     @Builder.Default
     private List<Comment> comments = new ArrayList<>();
